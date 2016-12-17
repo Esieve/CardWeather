47652c738640e1ae38a312f91b8ee8ff5468f039
张秦遥 <772392166@qq.com>
2016-12-17 13:51:55 +0800

    add now weather

--- popup.js
+++ popup.js
@@ -10,13 +10,29 @@ function httpRequest(url,callback) {
 }
 
 var weekday=new Array(7);
-weekday[0]="Sun";
-weekday[1]="Mon";
-weekday[2]="Tue";
-weekday[3]="Wed";
-weekday[4]="Thu";
-weekday[5]="Fri";
-weekday[6]="Sat";
+weekday[0]="Sunday";
+weekday[1]="Monday";
+weekday[2]="Tuesday";
+weekday[3]="Wednesday";
+weekday[4]="Thursday";
+weekday[5]="Friday";
+weekday[6]="Saturday";
+
+function showNowWeather(result) {
+	result = JSON.parse(result);
+	var id = result.weather[0].icon;
+	var img = "<img src='icons/"+id+".png'></img>";
+	document.getElementById("now_icon").innerHTML = img;
+
+	var d = new Date(result.dt*1000);
+	document.getElementById("now_week").innerHTML = weekday[d.getDay()];
+
+	var temp = result.main.temp;
+	document.getElementById("now_temp").innerHTML = Math.round(temp-273.15);
+
+	var city = result.name;
+	document.getElementById("now_city").innerHTML = city;
+}
 
 function showHourlyWeather(result) {
 	result = JSON.parse(result);
@@ -27,7 +43,7 @@ function showHourlyWeather(result) {
 		var id = list[i].weather[0].icon;
 		table += "<td><img src='icons/"+id+".png'></img></td>";
 		var d = new Date(list[i].dt*1000);
-		table += "<td>"+d.getHours()+"</td>";
+		table += "<td>"+d.getHours()+"h</td>";
 		table += "<td>"+Math.round(list[i].main.temp-273.15)+"°C</td>";
 		table += "</tr>";
 	}
@@ -48,7 +64,7 @@ function showDailyWeather(result){
 	table += "<tr>";
 	for(var i = 1;i<list.length;i++){
 		var d = new Date(list[i].dt*1000);
-		table += "<td>"+weekday[d.getDay()]+"</td>";
+		table += "<td>"+weekday[d.getDay()].substr(0,3)+"</td>";
 	}
 	table += "</tr>";
 	table += "<tr>";
@@ -63,6 +79,9 @@ function showDailyWeather(result){
 var city = localStorage.city;
 city = city?city:"beijing";
 var url;
+
+url = 'http://api.openweathermap.org/data/2.5/weather?q='+city+',china&lang=zh_cn&APPID=65d1bd8ddc51490ee98f0d478e91db85&cnt=5';
+httpRequest(url, showNowWeather);
 
 url = 'http://api.openweathermap.org/data/2.5/forecast?q='+city+',china&lang=zh_cn&APPID=65d1bd8ddc51490ee98f0d478e91db85&cnt=5';
 httpRequest(url, showHourlyWeather);
