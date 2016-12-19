4d2dcd9ef043241f8c530fad133cf623936e43ad
codesign-er <772392166@qq.com>
2016-12-19 21:17:45 +0800

    change the response of setting city

--- options.js
+++ options.js
@@ -1,6 +1,19 @@
 var city = (localStorage.city || "beijing");
 document.getElementById("city").value=city;
 document.getElementById("save").onclick = function () {
-	localStorage.city = document.getElementById("city").value;
-	alert("Success!");
+	var result = document.createElement("p");
+	result.id = "result";
+	if(/.*[\u4e00-\u9fa5]+.*$/.test(document.getElementById("city").value)){
+		result.innerText = "English only";
+		result.style.color = "#D0104C";
+	}else{
+		result.innerText = "Success!";
+		result.style.color = "#00AA90";
+		localStorage.city = document.getElementById("city").value;
+	}
+	var save = document.getElementById("save");
+	(document.getElementsByTagName("body"))[0].replaceChild(result,save);
+	setTimeout(function() {
+		(document.getElementsByTagName('body'))[0].replaceChild(save,document.getElementById("result"));
+	},1000);
 }
